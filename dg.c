83f5cd3e9634ad602e1576591afe522b6c614460
asherlie <lieber.asher@gmail.com>
2021-07-08 00:46:53 -0400

    added establish_connection(), which establishes a connection with a host

--- dg.c
+++ dg.c
@@ -5,12 +5,23 @@
 #include <string.h>
 #include <stdio.h>
 
-int main(){
+int establish_connection(char* host_name){
     int sock = socket(AF_INET, SOCK_STREAM, 0);
-    struct in_addr host_addr;
+    /*struct in_addr host_addr;*/
     struct hostent* h = gethostbyname("irc.freenode.net");
+    struct sockaddr_in addr;
 
-    memcpy(&host_addr, h->h_addr_list[0], h->h_length);
+    memcpy(&addr.sin_addr, h->h_addr_list[0], h->h_length);
+
     /*host_addr = */
-    puts(inet_ntoa(host_addr));
+    puts(inet_ntoa(addr.sin_addr));
+    if(connect(sock, (struct sockaddr*)&addr, sizeof(struct sockaddr_in))){
+        perror("connect");
+        return -1;
+    }
+
+    return sock;
+}
+
+int main(){
 }
