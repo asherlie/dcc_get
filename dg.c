00f13d256c2e6ed491e2244a6bb9ad2c406131df
asherlie <lieber.asher@gmail.com>
2021-07-08 01:29:05 -0400

    updated client

--- dg.c
+++ dg.c
@@ -3,25 +3,19 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <string.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 int establish_connection(char* host_name){
     int sock = socket(AF_INET, SOCK_STREAM, 0);
-    /*struct in_addr host_addr;*/
     struct hostent* h = gethostbyname(host_name);
     struct sockaddr_in addr = {.sin_family = AF_INET, .sin_port = htons(6660)},
-                 local_addr = {.sin_family = AF_INET, .sin_port = 0};// .sin_addr = INADDR_ANY};
+                 local_addr = {.sin_family = AF_INET, .sin_port = 0};
 
     local_addr.sin_addr.s_addr = INADDR_ANY;
-    /*
-     * local_addr.sin_family = AF_INET;
-     * local_addr.sin_port = 0;
-     * local_addr.sin_addr = INADDR_ANY;
-    */
 
     if(bind(sock, (struct sockaddr*)&local_addr, sizeof(struct sockaddr_in)) == -1)perror("bind");
-    /*listen(sock, 0);*/
 
     memcpy(&addr.sin_addr, h->h_addr_list[0], h->h_length);
 
@@ -34,6 +28,16 @@ int establish_connection(char* host_name){
     return sock;
 }
 
+#if 0
+TODO: 
+add a set of functionality that enables a list of startup commands that will be run in order,
+once a connection has been established
+
+USER malloc 0 * :malloc
+nick freeman
+PING :JmqXrjDYIj
+PONG JmqXrjDYIj     
+#endif
 int main(){
     int sock = establish_connection("irc.freenode.net"), llen;
     char* ln = NULL;
@@ -43,6 +47,12 @@ int main(){
     if(sock == -1)return 0;
 
     fp = fdopen(sock, "rw");
+
+    usleep(4000000);
+    /*
+     * fprintf(fp, "USER Goo 0 * :Goo\n");
+     * fprintf(fp, "NICK freeman\n");
+    */
 
     while((llen = getline(&ln, &sz, fp)) != -1){
         if(ln[llen-1] == '\n')ln[llen-1] = 0;
